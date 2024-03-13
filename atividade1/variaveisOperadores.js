13efbb7d14806e73c405293946229b25285bd90f
Gustavo Sales Piffer <127793227+piffer11@users.noreply.github.com>
2024-03-13 09:47:44 -0300

    Update variaveisOperadores.js

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,12 +1,1 @@
-const numA = 10;
-const numB = 5;
 
-const soma = numA + numB;
-const subtracao = numA - numB;
-const multiplicacao = numA * numB;
-const divisao = numA / numB;
-
-console.log(`Resultado da soma entre ${numA} e ${numB}: ${soma}`);
-console.log(`Resultado da subtração entre ${numA} e ${numB}: ${subtracao}`);
-console.log(`Resultado da multiplicação entre ${numA} e ${numB}: ${multiplicacao}`);
-console.log(`Resultado da divisão entre ${numA} e ${numB}: ${divisao}`);
